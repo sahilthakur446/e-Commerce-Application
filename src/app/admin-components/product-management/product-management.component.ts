c885091445b9a9f50dca9d41e79c3a9a64b6042b
sahilthakur446 <sahilthakur446@gmail.com>
2024-03-12 00:45:46 +0530

    added loading spinner

--- src/app/admin-components/product-management/product-management.component.ts
+++ src/app/admin-components/product-management/product-management.component.ts
@@ -8,16 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductManagementComponent implements OnInit{
   productList:any;
+  categoryList:any;
+  BrandList:any;
+  selectedCategory:string ="";
+  selectedBrand:string = "";
+  isLoading:boolean = false;
   constructor(private http: HttpClient) {
+  }
+
+  applyFilters()
+  {
+    this.isLoading = true
+    let url =  `https://localhost:7248/api/Brand/GetBrandWithProducts/${this.selectedBrand}`
+    setTimeout(() => {
+      this.http.get(url).subscribe({
+        next:(response:any) => {console.log(response)
+        this.productList = response.productDTOs
+      this.isLoading = false;},
+        error:(error) => {console.log(error)
+        this.isLoading =false},
+        complete:() => {
+          this.isLoading = false} 
+      })
+    }, 1000);
     
   }
+
   ngOnInit(): void {
     let url = "https://localhost:7248/api/Product/GetAllProducts"
     this.http.get(url).subscribe({
-      next:(response) => {console.log(response)
-      this.productList = response},
+      next:(response) => {this.productList = response},
       error:(error) => console.log(error),
       complete:() => console.log("Completed") 
     })
+
+    let categoryListUrl ="https://localhost:7248/api/Category/GetCategoryList";
+    let brandListUrl ="https://localhost:7248/api/Brand/GetBrandList";
+    this.http.get(categoryListUrl).subscribe((response:any) =>{this.categoryList = response});
+    this.http.get(brandListUrl).subscribe((response:any) =>{this.BrandList = response
+    console.log(this.BrandList)});
   }
 }
