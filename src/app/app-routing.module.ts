51e30eb2119d9e417e2fed6ff8b65866811bb193
sahilthakur446 <sahilthakur446@gmail.com>
2024-03-11 22:28:33 +0530

    Created product manager component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AddProductComponent } from './admin-components/add-product/add-product.component';
+import { ProductManagementComponent } from './admin-components/product-management/product-management.component';
 
 const routes: Routes = [
   {path:"Login",component:LoginComponent},
-  {path:"Login/Register",component:RegisterComponent}
-  // {path:"/",component:HomepageComponent}
+  {path:"Login/Register",component:RegisterComponent},
+  {path:"AddProduct",component:AddProductComponent},
+  {path:"ProductManager",component:ProductManagementComponent},
+  {path:"Home",component:HomepageComponent}
 ];
 
 @NgModule({
