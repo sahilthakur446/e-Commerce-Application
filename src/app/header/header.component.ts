a03bbec4405227ff5872b6b0b011d0b8d91f4271
sahilthakur446 <sahilthakur446@gmail.com>
2024-03-19 17:55:42 +0530

    modified header and homepage component added logout modal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../services/auth-service/auth.service';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user-profile-service/user.service';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -11,20 +12,16 @@ import { UserService } from '../services/user-profile-service/user.service';
 export class HeaderComponent implements OnInit{
 isLoggedIn:boolean = false;
 isSearchBarVisible:boolean = false;
+showLogoutModal:boolean = false;
+
 constructor(private authService:AuthService,private userService:UserService, private router:Router,) {
  }
 
  ngOnInit(): void {
-  this.userService.getIsLoggedIn().subscribe({
-    next: (response) => {
-      if (response == true) {
-        setTimeout(() => {
-          this.isLoggedIn = true;
-        }, 2100);
-      } else {
-        this.isLoggedIn = false;
-      }
-    }
+  this.userService.getIsLoggedIn().pipe(
+    delay(3000)
+  ).subscribe(isLoggedIn => { 
+    this.isLoggedIn = isLoggedIn;
   });
 }
 
@@ -40,5 +37,9 @@ logout(){
 
 openSearchBar(){
   this.isSearchBarVisible = !this.isSearchBarVisible;
+}
+
+toggleLogoutModal(){
+this.showLogoutModal = !this.showLogoutModal
 }
 }
